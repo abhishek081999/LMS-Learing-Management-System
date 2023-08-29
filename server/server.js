9cb327c47f04f859eee20f936f21fc2977bd0ee1
Abhishek Kumar <100945559+abhishek081999@users.noreply.github.com>
2023-08-29 17:18:16 +0530

    Mongodb connection added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 
 
 import app from './app.js';
+import connectionToDb from './config/dbConnection.js';
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
+    await connectionToDb();
 console.log(`App is running at http:localhost:${PORT}`);
 });
