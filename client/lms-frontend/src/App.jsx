41fe619eeb2de6d21ee41ed8702df49a658a8aee
Abhishek Kumar <100945559+abhishek081999@users.noreply.github.com>
2023-10-03 17:47:57 +0530

    Adding basic layout and  integrated to homepage

--- client/lms-frontend/src/App.jsx
+++ client/lms-frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css'
 
-// import { Routes } from 'react-router-dom'
-// import Footer from './Components/Footer'
+ import { Route,Routes } from 'react-router-dom'
+
+ import HomePage from './Pages/HomePage'
 
 function App() {
   
   return (
     <>
-     {/* <Routes> */}
+     <Routes>
      {/*<Route path="/" element={<Home/>}></Route> */}
-     {/* </Routes> */}
-     {/* <Footer/> */}
+     <Route path="/" element={<HomePage/>}></Route> 
+
+     </Routes>
+   
+   
     </>
   )
 }
