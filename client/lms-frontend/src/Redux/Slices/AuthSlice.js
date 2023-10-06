6304e71a4f359c10d90a79742d22f94aec3d45ab
Abhishek Kumar <100945559+abhishek081999@users.noreply.github.com>
2023-10-06 13:29:48 +0530

    Added complete Signup flow

--- client/lms-frontend/src/Redux/Slices/AuthSlice.js
+++ client/lms-frontend/src/Redux/Slices/AuthSlice.js
@@ -1,10 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
 
+import axiosInstance from "../../Helpers/axiosInstance"
 const initialState = {
-    isLoggedIn: localStorage.getItem('isLoggedIn')||false,
+    isLoggedIn: localStorage.getItem('isLoggedIn') || false,
     role: localStorage.getItem('role') || "",
-    data: localStorage.getItem('data') || {},
+    data: localStorage.getItem('data') || {}
 };
+
+export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
+    try {
+        const res = axiosInstance.post("user/register", data);
+        toast.promise(res, {
+            loading: "Wait! creating your account",
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: "Failed to create account"
+        });
+        return (await res).data;
+    } catch(error) {
+        toast.error(error?.response?.data?.message);
+    }
+})
 
 const authSlice = createSlice({
     name: 'auth',
@@ -12,5 +30,5 @@ const authSlice = createSlice({
     reducers: {},
 });
 
-//export const {} = authSlice.actions;
-export default authSlice.reducer
+// export const {} = authSlice.actions;
+export default authSlice.reducer;
