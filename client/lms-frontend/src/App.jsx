c27aa151fc39a3eaeb22b92364f3aa3dbfccb994
Abhishek Kumar <100945559+abhishek081999@users.noreply.github.com>
2023-10-09 11:22:08 +0530

    course list page completed

--- client/lms-frontend/src/App.jsx
+++ client/lms-frontend/src/App.jsx
@@ -3,11 +3,11 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 import AboutUs from './Pages/AboutUs';
+import CourseList from './Pages/Course/CourseList';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/Login';
 import NotFound from './Pages/NotFound';
 import Signup from './Pages/Signup';
-
 function App() {
 
   return (
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} ></Route>
         <Route path="/about" element={<AboutUs />} ></Route>
-
+        <Route path="/courses" element={<CourseList />} ></Route>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
 
